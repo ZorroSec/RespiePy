c62ab37a089de025165d1a8e51510fcfb52fbd5b
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-14 09:23:27 -0300

    testando primeiro o cep la no discord

--- app/components/cep.py
+++ app/components/cep.py
@@ -1,3 +1,9 @@
 from app import bot
 from json import loads
 import requests
+
+@bot.command(name='cep')
+async def cep(ctx, cep):
+    req = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+    req = loads(req.text)
+    await ctx.send(req)
