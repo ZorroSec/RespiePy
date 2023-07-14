a76f46586bba620c6f83204f3c0af8dc9f83f772
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-14 13:07:28 -0300

    adicionando os dois endpoints que faltavam para funcionar o bot completamente

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,5 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 from app.components import test
 from app.components import fox
 from app.components import cep
+from app.components import cnpj
+from app.components import address
