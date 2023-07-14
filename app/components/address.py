827a6047e9b7f6f58b0188f81c22e127ea79b18e
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-14 12:49:46 -0300

    esqueci de adicionar um elemento muito importante

--- app/components/address.py
+++ app/components/address.py
@@ -2,6 +2,7 @@ from app import bot
 import requests
 from json import loads
 
+@bot.command(name='address')
 async def address(ctx, address):
     req = requests.get(f'http://ip-api.com/json/{address}')
     req = loads(req.text)
