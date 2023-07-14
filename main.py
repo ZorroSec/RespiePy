4360e8c59911ca233503dba276e50790f54ac8c8
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-14 13:09:00 -0300

    resetando otoken do bot

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ import requests
 
 if __name__ == '__main__':
     os.system('clear||cls')
-    bot.run('MTEyOTE4MzAyNjExMTMyMDExNA.GbNwXD.RVrxuneNM7UZBmIiXm_9pXqBNkmaB_qLw4QkOg')
+    bot.run('MTEyOTE4MzAyNjExMTMyMDExNA.G7D7RT.QD_w9VcpOlVV4MtVuo4VYvMlr6HVMn4lQrTbt4')
