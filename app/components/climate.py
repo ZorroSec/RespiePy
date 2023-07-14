5759166c824b5124c42bdd205afab8fd825ea87e
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-14 19:13:59 -0300

    climate terminado

--- app/components/climate.py
+++ app/components/climate.py
@@ -2,6 +2,7 @@ from app import bot
 import requests
 from json import loads
 
+@bot.command(name='climate')
 async def climate(ctx, cityCode):
     req = requests.get(f'https://brasilapi.com.br/api/cptec/v1/clima/previsao/{cityCode}')
     req = loads(req.text)
