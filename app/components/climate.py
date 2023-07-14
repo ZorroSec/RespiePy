a4c3e22e55f4629a2c6cc956bc56dedf5cca87ed
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-14 14:27:52 -0300

    terminando o endpoint climate

---
+++ app/components/climate.py
@@ -0,0 +1,8 @@
+from app import bot
+import requests
+from json import loads
+
+async def climate(ctx, cityCode):
+    req = requests.get(f'https://brasilapi.com.br/api/cptec/v1/clima/previsao/{cityCode}')
+    req = loads(req.text)
+    await ctx.send(req)
