5ef839e1d94b8a2f35288fca53f6377b3f0cac11
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-15 19:23:50 -0300

    fazendo com que o cepv2 se transforme realmente em um comando

--- app/components/cep-v2.py
+++ app/components/cep-v2.py
@@ -2,6 +2,7 @@ from app import bot
 import requests
 from json import loads
 
+@bot.command(name='cepv2')
 async def cepv2(ctx, cep):
     req = requests.get(f'https://brasilapi.com.br/api/cep/v2/{cep}')
     req = loads(req.text)
